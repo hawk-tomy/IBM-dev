84586ee353c7fe1c3d75e99cc1eaccc054e3a3ed
hawk  tomy <hawk201000211101@gmail.com>
2020-06-27 22:39:43 +0000

    Update user.py add command&command group
    discord.ext楽

--- src/pack/bank/user.py
+++ src/pack/bank/user.py
@@ -17,6 +17,32 @@ class User(commands.Cog):
     async def ping(self, ctx):
         await ctx.send('pong')
         logger.info('ping&pong')
+    
+    @commands.command()
+    async def in(self, ctx):
+        await ctx.send('in')
+        logger.info('in')
+    
+    @commands.command()
+    async def out(self, ctx):
+        await ctx.send('out')
+        logger.info('out')
+    
+    @commands.group()
+    async def show(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('show')
+            logger.info('show')
+    
+    @show.command()
+    async def deposit(self, ctx):
+        await ctx.send('show deposit')
+        logger.info('show deposit')
+    
+    @show.command()
+    async def log(self, ctx):
+        ctx.send('log')
+        logger.info('log')
 
 def setup(bot):
     bot.add_cog(User(bot))
