47e548d141a9677771aed78459ffd19b4ecabdb3
hawk  tomy <hawk201000211101@gmail.com>
2020-06-29 10:03:58 +0000

    Update admin.py add new cog

--- src/pack/bank/admin.py
+++ src/pack/bank/admin.py
@@ -1,1 +1,18 @@
+import logging
 
+import discord
+from discord.ext import commnads
+import yaml
+
+from pack.util import myfunction as MF
+
+logger = logging.getLogger('bot').getChild(__name__)
+
+class Admin(comannds.cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
+    logger.info('add_cog_success')
