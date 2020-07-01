e42db77bbb2d016dc0fe2e0caf4dad1c2eb3a757
hawk  tomy <hawk201000211101@gmail.com>
2020-07-01 13:12:47 +0000

    Update myfunction.py add function

--- src/util/myfunction.py
+++ src/util/myfunction.py
@@ -2,10 +2,12 @@ import logging
 
 logging.getLogger('discord')
 logger = logging.getLogger('bot').getChild('myfunction')
+formatter= logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def getLogger(name, level= logging.DEBUG, saveName= 'mainnoname.log',path='.'):
-    #フォーマットの定義
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    if '.' in name:
+        raise NameError('name is not root')
+        return
     #ロガーの定義
     def_logger = logging.getLogger(name)
     def_logger.setLevel(level)
@@ -14,17 +16,22 @@ def getLogger(name, level= logging.DEBUG, saveName= 'mainnoname.log',path='.'):
     fh.setFormatter(formatter)
     fh.setLevel(logging.NOTSET)
     def_logger.addHandler(fh)
-    if not '.' in name:
-        #最上位ロガー専用
-        warningSaveName = path + '/' + 'warning' + saveName
-        fhw = logging.FileHandler(warningSaveName, encoding='utf-8')
-        fhw.setFormatter(formatter)
-        fhw.setLevel(logging.WARNING)
-        #コンソール出力用
-        sh = logging.StreamHandler()
-        sh.setFormatter(formatter)
-        sh.setLevel(logging.NOTSET)
-        #それぞれロガーに追加
-        def_logger.addHandler(sh)
-        def_logger.addHandler(fhw)
+    #最上位ロガー専用
+    warningSaveName = path + '/' + 'warning' + saveName
+    fhw = logging.FileHandler(warningSaveName, encoding='utf-8')
+    fhw.setFormatter(formatter)
+    fhw.setLevel(logging.WARNING)
+    #コンソール出力用
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    sh.setLevel(logging.NOTSET)
+    #それぞれロガーに追加
+    def_logger.addHandler(sh)
+    def_logger.addHandler(fhw)
     return def_logger
+
+def getChild(root,child,file):
+    def_logger= logging.getLogger(root).getChild(child)
+    fh = logging.FileHandler(file)
+    fh.setFormatter(formatter)
+    
