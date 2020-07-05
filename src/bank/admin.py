3455ed042f9d3c3525edf3d7f190f224cdb6ec27
hawk_tomy <hawk201000211101@gmail.com>
2020-07-05 10:28:11 +0900

    update 銀行作成コマンドの実装開始

--- src/bank/admin.py
+++ src/bank/admin.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import re
 
 import discord
 from discord.ext import commands
@@ -7,6 +9,12 @@ import yaml
 from src.util import myfunction as MF
 
 logger = logging.getLogger('bot').getChild(__name__)
+
+with open('data/data.yaml','r',encoding='utf-8')as f:
+    data = yaml.safe_load(f)
+
+with open('data/config.cfg','r',encoding='utf-8')as f:
+    config = yaml.safe_load(f)
 
 class Admin(commands.Cog):
     '''
@@ -42,8 +50,16 @@ class Admin(commands.Cog):
     @bank.command()
     @commands.has_permissions(administrator=True)
     async def make(self, ctx, *, arg):
-        await ctx.send('make : '+ str(arg))
-        logger.info('make :' + str(arg))
+        if re.search(r'[\\/:*?"<>|]',arg):
+            await ctx.send('使用不可能な文字が含まれています。`\/:*?"<>|`以外を使用してください。')
+        else:
+            data['bank_index'] = {ctx.guild.id: str(arg)}
+            with open('data/data.yaml','w',encoding='utf-8')as f:
+                yaml.dump(data, f, encoding='utf-8', allow_unicode=True)
+            os.mkdir('data/' + str(arg))
+
+            await ctx.send('make : '+ str(arg))
+            logger.info('make :' + str(arg))
 
     @make.error
     async def make_error(self, ctx, error):
