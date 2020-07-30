cfd8238be14a87b2d9fd9b37aa8730294069558d
tomy <67221751+hawk-tomy@users.noreply.github.com>
2020-07-31 07:07:41 +0900

    update account

--- src/bank/account.py
+++ src/bank/account.py
@@ -4,7 +4,7 @@ import discord
 from discord.ext import commands
 import yaml
 
-from src.util import myfunction as MF
+from ..util import MF
 
 root= logging.getLogger('bot')
 logger = root.getChild(__name__)
